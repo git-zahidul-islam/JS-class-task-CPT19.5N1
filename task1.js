5cca9b50a4aad352e26ab3fd8713f485773809b5
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-02-01 12:24:12 +0600

    reverse colors use for of

--- task1.js
+++ task1.js
@@ -1,11 +1,20 @@
-// task 01
+// // task 01
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+
+// // console.log(colors.reverse())
+
+// const revColors = [];
+// for (let i = colors.length -1; i >= 0; i--) {
+//     let color = colors[i]
+//     revColors.push(color)
+// };
+// console.log(revColors);
+
+// reverse use for of 
 const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
 
-// console.log(colors.reverse())
-
 const revColors = [];
-for (let i = colors.length -1; i >= 0; i--) {
-    let color = colors[i]
-    revColors.push(color)
-};
+for (let color of colors) {
+    revColors.unshift(color)
+}
 console.log(revColors);
