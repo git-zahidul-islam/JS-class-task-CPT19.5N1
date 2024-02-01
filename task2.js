0a7608979c532b68bf222990c74521c6c6b65e05
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-02-01 12:44:04 +0600

    task complete

---
+++ task2.js
@@ -0,0 +1,10 @@
+// task 2 even number 
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+
+for (let i = 0; i < numbers.length; i++) {
+    let even = numbers[i];
+    if (even % 2 === 0) {
+        let evenShow = even;
+        console.log(evenShow);
+    }
+}
