ff48a1b16bd232197dc01f640e7ad1cc3e512699
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-02-01 12:19:31 +0600

    complete the task

--- task1.js
+++ task1.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+// task 01
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+
+// console.log(colors.reverse())
+
+const revColors = [];
+for (let i = colors.length -1; i >= 0; i--) {
+    let color = colors[i]
+    revColors.push(color)
+};
+console.log(revColors);
